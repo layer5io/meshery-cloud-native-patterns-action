556343165a0f9e8a6982399e0691fdcc1013c9fa
sayantan1413 <sayantanbose2001@gmail.com>
2021-10-25 21:32:40 +0530

    Added the main.sh
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

---
+++ main.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || realpath "${BASH_SOURCE[0]}")")
+
+main() {
+	get_dependencies
+
+	setupArgs=()
+	if [[ -n "${INPUT_PROVIDER_TOKEN:-}" ]]; then
+		setupArgs+=(--provider-token ${INPUT_PROVIDER_TOKEN})
+	fi
+
+	if [[ -n "${INPUT_PLATFORM:-}" ]]; then
+		setupArgs+=(--platform ${INPUT_PLATFORM})
+	fi
+
+	"$SCRIPT_DIR/meshery.sh" "${setupArgs[@]}"
+
+	commandArgs=()
+	if [[ -n "${INPUT_PATTERN_FILENAME:-}" ]]; then
+		commandArgs=(--pattern-filename ${INPUT_PATTERN_FILENAME})
+	fi
+
+	if [[ -n "${INPUT_PATTERN_NAME:-}" ]]; then
+		commandArgs=(--pattern-name ${INPUT_PATTERN_NAME})
+	fi
+
+	"$SCRIPT_DIR/mesheryctl.sh" "${commandArgs[@]}"
+}
+
+get_dependencies() {
+	sudo wget https://github.com/mikefarah/yq/releases/download/v4.10.0/yq_linux_amd64 -O /usr/bin/yq --quiet
+	sudo chmod +x /usr/bin/yq
+}
+
+main
