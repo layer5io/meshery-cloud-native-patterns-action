1fa36f609478bb5e2ceb7422cd14e13f736572c9
sayantan1413 <sayantanbose2001@gmail.com>
2021-11-01 20:44:20 +0530

    Update 7
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

--- mesheryctl.sh
+++ mesheryctl.sh
@@ -2,6 +2,19 @@
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || realpath "${BASH_SOURCE[0]}")")
 
+declare -A adapters
+adapters["istio"]=meshery-istio:10000
+adapters["linkerd"]=meshery-linkerd:10001
+adapters["consul"]=meshery-consul:10002
+adapters["octarine"]=meshery-octarine:10003
+adapters["nsm"]=meshery-nsm:10004
+adapters["network_service_mesh"]=meshery-nsm:10004
+adapters["kuma"]=meshery-kuma:10007
+adapters["cpx"]=meshery-cpx:10008
+adapters["osm"]=meshery-osm:10009
+adapters["open_service_mesh"]=meshery-osm:10009
+adapters["traefik-mesh"]=meshery-traefik-mesh:10006
+adapters["traefik_mesh"]=meshery-traefik-mesh:10006
 
 main() {
 
@@ -12,12 +25,14 @@ main() {
 
 	if [ -z "$pattern_name" ]
 	then
+		shortName=$(echo ${adapters["$service_mesh"]} | cut -d '-' -f2 | cut -d ':' -f1)
 		docker network connect bridge meshery_meshery_1
 		docker network connect minikube meshery_meshery_1
 		docker network connect bridge meshery_meshery-"$shortName"_1
 		docker network connect minikube meshery_meshery-"$shortName"_1
 
 		mesheryctl system config minikube -t ~/auth.json
+		docker ps
 		mesheryctl pattern apply --file https://raw.githubusercontent.com/service-mesh-patterns/service-mesh-patterns/master/samples/IstioFilterPattern.yaml -t ~/auth.json
 
             
